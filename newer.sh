c2077ca5f16dad54b62e8f42385f6e322c97d6e4
Dng-Dng <61781633+Dng-Dng@users.noreply.github.com>
2020-03-17 18:31:55 +1100

    Delete newer.sh

--- newer.sh
+++
@@ -1,5 +0,0 @@
-#'find' looks for name of argument entered in current directory
-#ls -t list all files in the directory from time modified descending order (latest to oldest)
-#head -1 prints first file in output 
- 
-find . -name $1 | ls -t list | head -1
