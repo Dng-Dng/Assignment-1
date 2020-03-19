7353b731a3816863d9e7c9942860bb642f93f2a5
Dng-Dng <61781633+Dng-Dng@users.noreply.github.com>
2020-03-19 11:47:18 +1100

    Update newer.sh

--- newer.sh
+++ newer.sh
@@ -1,7 +1,9 @@
 #Question 1
+#if condition checks if a filename was inputted
+#if filename detected,
 #ls -t list all files in inputted argument ('$@') and sort by time modified
 #head -1 prints latest modified file from sorted arguments
-
+#if nothing detected, print 'No arguments provided'
 #!/bin/bash
 if [ $# -gt 0 ]; then
 	ls -t "$@" | head -1
